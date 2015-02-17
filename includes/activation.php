dabb7d006b343d404239662985cb879a4b5d04e8
Vitaliy Lebedev <malgin05@gmail.com>
2015-02-17 23:45:29 +0200

    Fix SQL for creating donations table

--- includes/activation.php
+++ includes/activation.php
@@ -15,8 +15,8 @@ function dwr_activate_plugin()
     $sql =
         "
             CREATE TABLE IF NOT EXISTS `" . $table_donations . "` (
-                `id` INT(11) UNIQUE UNSIGNED NOT NULL AUTO_INCREMENT,
-                `robokassa_id` INT(11) UNIQUE UNSIGNED DEFAULT NULL,
+                `id` INT(11) UNSIGNED UNIQUE NOT NULL AUTO_INCREMENT,
+                `robokassa_id` INT(11) UNSIGNED UNIQUE DEFAULT NULL,
                 `amount` FLOAT UNSIGNED NOT NULL,
                 `currencyLabel` VARCHAR(255) NOT NULL,
                 `currencyName` VARCHAR(255) NOT NULL,
